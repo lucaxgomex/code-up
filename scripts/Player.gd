4b204e90814dc1f90c2aa4653ee0a5aa2fc2d4c8
lucaxgomex <lg.projectdev@gmail.com>
2022-05-23 23:42:01 -0300

    Adding movement settings

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,4 +6,12 @@ var gravity = 1200
 var jumpForce = -720
 
 func _physics_process(delta: float) -> void:
-	var moveDirection = int(Input.is_action_pressed())
+	velocity.y += gravity * delta
+	var moveDirection = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	
+	if Input.is_action_pressed("jump"):
+		velocity.y = jumpForce / 2
+	
+	velocity.x = moveSpeed * moveDirection
+	
+	move_and_slide(velocity / 2)
